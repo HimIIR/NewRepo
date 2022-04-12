72a385465632c723eb716084d197a963e20e4aea
HimIIR <h_kumar3@ce.iitr.ac.in>
2022-04-12 23:50:31 +0530

    firtst commit

---
+++ game.py
@@ -0,0 +1,6 @@
+Do
+msg1 = msgbox("colse this box",o,"Python.Hub")
+msg2 = msgbox("try again",o,"Python.Hub")
+msg3 = msgbox("try again",o,"python.Hub")
+msg4 = msgbox("congo.. you can't close this sit",o, "Python.Hub")
+Loop
